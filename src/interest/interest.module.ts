1e8e2f38184bec1752d178491e8cb90b0308303f
lfer <firassaada1111@gmail.com>
2023-04-28 15:33:09 +0100

    rest_tasks

--- src/interest/interest.module.ts
+++ src/interest/interest.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { InterestService } from './interest.service';
 import { InterestController } from './interest.controller';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {CoupleEntity} from "../models/couple.entity";
+import {PersonEntity} from "../models/person.entity";
+import {CoupleModule} from "../couple/couple.module";
+import {InterestEntity} from "../models/interest.entity";
 
 @Module({
+  imports : [ TypeOrmModule.forFeature([InterestEntity]),CoupleModule] ,
   controllers: [InterestController],
   providers: [InterestService]
 })
--- src/invitation/invitation.controller.ts
+++ src/invitation/invitation.controller.ts
@@ -1,9 +1,10 @@
-import {Body, Controller, Post, UseGuards} from '@nestjs/common';
+import {BadRequestException, Body, Controller, Param, ParseIntPipe, Post, UseGuards} from '@nestjs/common';
 import { InvitationService } from './invitation.service';
 import {JwtAuthGuard} from "../couple/guard/jwt-auth.guard";
 import {SendRequestDto} from "./dto/send-request.dto";
 import {Couple} from "../decorators/couple.decorator";
 import {CoupleEntity} from "../models/couple.entity";
+import {RequestStatus} from "../models/request.entity";
 
 @UseGuards(JwtAuthGuard)
 @Controller('invitation')
@@ -13,8 +14,16 @@ export class InvitationController {
 
 
   @Post("send")
-  sendRequest(@Couple() sender: Partial<CoupleEntity> ,@Body() requestDto : SendRequestDto){
-    return this.invitationService.sendInvitation(sender,requestDto)
+  // @Couple() ????,
+  sendRequest(@Couple() sender: Partial<CoupleEntity> ,@Body() requestDto : SendRequestDto)
+  {
+    return this.invitationService.sendInvitation(sender,requestDto) ;
+  }
+  @Post("request/:requesTId")
+  async respondToRequest
+  (@Param('requestId',ParseIntPipe) requestId : number, @Body('response') response : RequestStatus )
+  {
+   return this.invitationService.respondToRequest(requestId,response) ;
   }
 
 }
